aef4b22c246c5305d952ce4b5337eb0cfbe9acd6
luvips <luviaamagaali07@gmail.com>
2025-10-25 01:46:41 -0600

    4 componentes visuales principales

---
+++ src/app/components/audio-controller/audio-controller.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-controller',
+  imports: [],
+  templateUrl: './audio-controller.html',
+  styleUrl: './audio-controller.css',
+})
+export class AudioController {
+
+}
---
+++ src/app/components/song-info/song-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-info',
+  imports: [],
+  templateUrl: './song-info.html',
+  styleUrl: './song-info.css',
+})
+export class SongInfo {
+
+}
