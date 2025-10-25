a31c4bbe5b3ff4038d70b01a027aa18da9b3d008
luvips <luviaamagaali07@gmail.com>
2025-10-25 00:48:41 -0600

    añadir los environments para la config

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const spotify_environments={
+    API_URL: 'https://api.spotify.com/v1',
+    AUTH_API_URL: 'https://accounts.spotify.com/api/token',
+    CLIENT_ID: 'c4eebe2cb7fd418aa1c2129b559aae2d',
+    CLIENT_SECRET: '2bf6527b2c2743658155827e30826cf4',
+}
+
+export const bd_conection = {}
