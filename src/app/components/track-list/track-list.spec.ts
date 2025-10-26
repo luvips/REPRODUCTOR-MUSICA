a52e0ccd89c0aefeb5ed9f4fe54ba72132b22de9
luvips <luviaamagaali07@gmail.com>
2025-10-26 08:52:15 +0000

    add TrackListComponent with styles

---
+++ src/app/components/track-list/track-list.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrackListComponent } from './track-list';
+
+describe('TrackListComponent', () => {
+  let component: TrackListComponent;
+  let fixture: ComponentFixture<TrackListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TrackListComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TrackListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
